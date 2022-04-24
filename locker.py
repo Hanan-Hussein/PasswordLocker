be7e032f061a5dfb0eb88f3f1550d40ad0c8806d
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-24 13:42:36 +0300

    authenticate-Authenticate created user

--- locker.py
+++ locker.py
@@ -12,3 +12,19 @@ if __name__ == "__main__":
     new_user = User(user_username, user_password)
     print("\u001b[32m","ACCOUNT SUCCESSFULLY CREATED","\u001b[0m")    
     print()
+    print("Login to Your Account :")
+   
+    while True:
+        print()
+        user_username_verification = input("USERNAME : ")
+        print()
+        user_password_verification = input("PASSWORD : ")
+        print()
+        authentification = new_user.authenticate(user_username_verification, user_password_verification)
+        if authentification:
+            print("\u001b[32m","SUCCESSFULL LOGIN","\u001b[0m")
+            print()
+            break
+        else:
+            print("\u001b[31m","WRONG CREDENTIALS\n","\u001b[0m")
+        print("TRY AGAIN")
