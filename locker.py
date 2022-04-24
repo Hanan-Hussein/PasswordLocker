8fd0ad922ade0dcdaa4d630d0cde5dfd500cdbf4
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-24 13:38:58 +0300

    ft:banner-created the welcome banner

---
+++ locker.py
@@ -0,0 +1,7 @@
+from vault import User, Credentials
+if __name__ == "__main__":
+
+    bannner = "WELCOME TO PASSWORD LOCKER"
+    print("\u001b[4m", f"{bannner:^1000}", "\u001b[0m", "\n")
+    input("Press enter to create an account with a username and password : ")
+    print()
